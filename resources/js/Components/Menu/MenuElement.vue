26572c2261fbc702ad9d9501a4275fc2822b6c8c
Alejandro Méndez <alejmendez.87@gmail.com>
2024-04-08 23:24:20 -0400

    center icons

--- resources/js/Components/Menu/MenuElement.vue
+++ resources/js/Components/Menu/MenuElement.vue
@@ -24,7 +24,9 @@ const props = defineProps({
     class="menu-element inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium text-[#7B849C] rounded-md border-gray-200 hover:bg-[#FFEBE2] hover:text-[#ED3900] transition duration-400 ease-in-out"
     :class="{ active: props.active }"
   >
-    <FontAwesomeIcon :icon="props.icon" class="mr-2 text-xl" />
+    <div class="w-[25px] mr-2 flex justify-center">
+      <FontAwesomeIcon :icon="props.icon" class="text-xl" />
+    </div>
     {{ props.text }}
   </Link>
 </template>
