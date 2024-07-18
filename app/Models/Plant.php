ec70d8667b82edd6191f58103c749fe4d6e49614
Alejandro Méndez <alejmendez.87@gmail.com>
2024-07-17 22:57:30 -0400

    add mutator plant

--- app/Models/Plant.php
+++ app/Models/Plant.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOneThrough;
@@ -38,4 +39,13 @@ class Plant extends Model
     {
         return $this->belongsTo(PlantType::class);
     }
+
+    protected function code(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => strtoupper(trim($value)),
+            set: fn (string $value) => strtoupper(trim($value)),
+        );
+    }
+
 }
