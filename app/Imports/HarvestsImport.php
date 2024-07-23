e8b663858ce2fbb1c13eb70d7d0d226826800c5f
Alejandro Méndez <alejmendez.87@gmail.com>
2024-07-22 22:46:12 -0400

    fix rowCount on HarvestsImport

--- app/Imports/HarvestsImport.php
+++ app/Imports/HarvestsImport.php
@@ -9,14 +9,19 @@ use Illuminate\Support\Str;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Concerns\RegistersEventListeners;
+use Maatwebsite\Excel\Events\AfterImport;
+use Illuminate\Support\Facades\Validator;
 use App\Services\Plants\FindPlantByCode;
 
 use Illuminate\Validation\Rule;
 
-class HarvestsImport implements ToModel, WithHeadingRow
+class HarvestsImport implements ToModel, WithHeadingRow, WithEvents
 {
-    use Importable;
+    use Importable, RegistersEventListeners;
 
+    private $rowCount = 0;
     protected $harvest_id;
 
     public function __construct($harvest_id) {
@@ -25,14 +30,15 @@ class HarvestsImport implements ToModel, WithHeadingRow
 
     public function model(array $row)
     {
-        $rows = $rows->toArray();
         $row['codigo_de_planta'] = strtoupper(trim($row['codigo_de_planta']));
 
-        Validator::make($rows, [
+        Validator::make($row, [
             'codigo_de_planta' => 'required|exists:plants,code',
             'calidad' => 'max:30|nullable',
             'peso' => 'required|numeric|between:0,99999',
         ])->validate();
+
+        $this->rowCount++;
 
         return new HarvestDetail([
             'harvest_id' => $this->harvest_id,
@@ -45,5 +51,16 @@ class HarvestsImport implements ToModel, WithHeadingRow
     public function getPlantIdByCode($code)
     {
         return optional(FindPlantByCode::call($code), fn (Plant $plant) => $plant->id);
+    }
+
+    public static function afterImport(AfterImport $event)
+    {
+        $import = $event->getConcernable();
+        session(['rowCount' => $import->rowCount]);
+    }
+
+    public function getRowCount()
+    {
+        return $this->rowCount;
     }
 }
