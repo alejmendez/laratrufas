cb0b4967e1bb1516074667090a12c172f81c112f
Alejandro Méndez <alejmendez.87@gmail.com>
2024-08-20 21:16:21 -0400

    fix remember checkbox on login

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -63,7 +63,7 @@ const submit = () => {
 
             <div class="block mt-4">
                 <label class="flex items-center">
-                    <Checkbox name="remember" v-model:checked="form.remember" />
+                    <Checkbox name="remember" v-model="form.remember" />
                     <span class="ms-2 text-sm text-gray-600">{{ $t('auth.login.form.remember_me') }}</span>
                 </label>
             </div>
