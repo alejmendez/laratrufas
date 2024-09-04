ae6396dd99524f8051c7b90f8f8406dd6abe191c
Alejandro Méndez <alejmendez.87@gmail.com>
2024-09-03 21:48:12 -0400

    change deploy sh

--- deploy.sh
+++ deploy.sh
@@ -11,3 +11,5 @@ npm run build
 docker compose up -d
 docker exec laratrufa_app composer install --optimize-autoloader --no-dev
 docker exec laratrufa_app php artisan optimize
+docker exec laratrufa_app php artisan migrate --force
+docker exec laratrufa_app php artisan app:sync-permissions
