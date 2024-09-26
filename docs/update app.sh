72e73700456304e38f8fd0a5059fb15c3b158ff9
Alejandro Méndez <alejmendez.87@gmail.com>
2024-09-26 01:31:58 -0300

    update doc

--- docs/update app.sh
+++ docs/update app.sh
@@ -1,8 +1,5 @@
 sudo systemctl stop nginx ; sudo systemctl stop php8.3-fpm ; sudo systemctl stop octane
 
-git pull
-composer install --optimize-autoloader --no-dev
-npm run build
-php artisan optimize
+git pull ; composer install --optimize-autoloader --no-dev ; npm run build -- -l silent ; php artisan optimize
 
 sudo systemctl start octane ; sudo systemctl start php8.3-fpm ; sudo systemctl start nginx
