deef2c50bda1566d01ed4681040d6d219534d16b
Alejandro Méndez <alejmendez.87@gmail.com>
2024-12-15 20:29:42 -0300

    update docs

--- docs/EC2 install.sh
+++ docs/EC2 install.sh
@@ -111,5 +111,12 @@ ALTER USER postgres WITH PASSWORD 'postgres';
 CREATE DATABASE laratrufas;
 
 
-echo 'alias update_app="git pull ; composer install --optimize-autoloader --no-dev ; npm run build ; php artisan optimize"' >> ~/.bashrc
+echo -e '\nif [ -f ~/.bash_aliases ]; then\n    . ~/.bash_aliases\nfi\n' >> ~/.bashrc
+
+touch ~/.bash_aliases
+echo 'alias stop_services="sudo systemctl stop nginx ; sudo systemctl stop php8.3-fpm ; sudo systemctl stop octane"' >> ~/.bash_aliases
+echo 'alias start_services="sudo systemctl start octane ; sudo systemctl start php8.3-fpm ; sudo systemctl start nginx"' >> ~/.bash_aliases
+echo 'alias update_app_with_migrations="stop_services ; git pull ; npm run build -- -l silent ; composer install --optimize-autoloader --no-dev ; php artisan migrate --force ; php artisan optimize ; start_services"' >> ~/.bash_aliases
+echo 'alias update_app="stop_services ; git pull ; npm run build -- -l silent ; composer install --optimize-autoloader --no-dev ; php artisan optimize ; start_services"' >> ~/.bash_aliases
+
 source ~/.bashrc
