a69ede564b3c4c9badbf17ccbcd3a254e6f47e38
Alejandro Méndez <alejmendez.87@gmail.com>
2024-12-15 16:39:31 -0300

    add aliases docs

--- docs/update app.sh
+++
@@ -1,5 +0,0 @@
-sudo systemctl stop nginx ; sudo systemctl stop php8.3-fpm ; sudo systemctl stop octane
-
-git pull ; composer install --optimize-autoloader --no-dev ; npm run build -- -l silent ; php artisan optimize
-
-sudo systemctl start octane ; sudo systemctl start php8.3-fpm ; sudo systemctl start nginx
