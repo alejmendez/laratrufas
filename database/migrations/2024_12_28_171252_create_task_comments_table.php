b8cefb061c9667dfdfa4fcb4f7790e35be3f55ee
Alejandro Méndez <alejmendez.87@gmail.com>
2024-12-28 14:18:15 -0300

    Add TaskComment model and migration for task_comments table
    - Created TaskComment model with relationships to User and Task.
    - Added migration to create task_comments table, including foreign keys for user_id and task_id.
    - Migrated existing comments from tasks to task_comments and removed comments column from tasks.

---
+++ database/migrations/2024_12_28_171252_create_task_comments_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('task_comments', function (Blueprint $table) {
+            $table->id();
+            $table->text('comments')->nullable();
+
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->integer('task_id')->unsigned()->nullable();
+            $table->foreign('task_id')->references('id')->on('tasks')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+
+        $tasks = DB::table('tasks')->whereNotNull('comments')->get();
+        foreach ($tasks as $task) {
+            DB::table('task_comments')->insert([
+                'comments' => $task->comments,
+                'user_id' => $task->responsible_id,
+                'task_id' => $task->id,
+                'created_at' => $task->created_at,
+                'updated_at' => $task->updated_at
+            ]);
+        }
+
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('comments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('task_comments');
+
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->text('comments')->nullable();
+        });
+    }
+};
