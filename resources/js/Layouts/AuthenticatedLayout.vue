6f4fa7e7ab96bb2eb6aff0cf02247e11f35cad59
Alejandro Méndez <alejmendez.87@gmail.com>
2025-04-09 21:46:51 -0400

    Actualizar estilos en AuthenticatedLayout.vue para mejorar la consistencia visual. Se modificaron las clases de color y fondo para utilizar la sintaxis de paréntesis en lugar de la notación de color hexadecimal, alineándose con los cambios recientes en otros componentes.

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -24,10 +24,10 @@ const menuRightItems = menuElementsRight(currentComponent);
 </script>
 
 <template>
-  <div class="flex items-center w-full h-[64px] bg-gray-50 dark:bg-[#2F3349] text-gray-100 px-[20px] py-[10px] z-30">
+  <div class="flex items-center w-full h-[64px] bg-gray-50 dark:bg-(#2F3349) text-gray-100 px-[20px] py-[10px] z-30">
     <div class="w-[230px]">
       <div
-        class="w-[36px] inline rounded outline-none transition duration-75 text-[color:--p-primary-500] hover:text-[color:--p-primary-700] focus-visible:ring-primary-600 border border-[color:--p-primary-500] hover:border-[color:--p-primary-700] px-3 py-1"
+        class="w-[36px] inline rounded outline-none transition duration-75 text-(color:--p-primary-500) hover:text-(color:--p-primary-700) focus-visible:ring-primary-600 border border-(color:--p-primary-500) hover:border-(color:--p-primary-700) px-3 py-1"
         @click="sideBarStore.toggle"
       >
         <FontAwesomeIcon icon="fa-solid fa-bars" />
@@ -40,16 +40,16 @@ const menuRightItems = menuElementsRight(currentComponent);
   </div>
   <div class="w-full flex font-normal text-gray-900 antialiased">
     <div
-      class="flex-none transition-all duration-200 ease-out z-10 bg-gray-50 dark:bg-[#2F3349] text-gray-100"
+      class="flex-none transition-all duration-200 ease-out z-10 bg-gray-50 dark:bg-(#2F3349) text-gray-100"
       :class="{ 'lg:w-[320px] lg:opacity-100 w-[0px] opacity-0': !showSideBar, 'lg:w-[0px] lg:opacity-0 w-full opacity-100': showSideBar }"
     >
       <h3 class="font-bold text-xl inline ms-6 mt-4">
-        <span class="text-[color:--p-primary-color]">SW </span>
+        <span class="text-(color:--p-primary-color)">SW </span>
         <span class="text-gray-900 dark:text-gray-50">Agricola</span>
       </h3>
       <SideBarLeft />
     </div>
-    <div class="grow z-20 bg-zinc-200 dark:bg-[#1D2132]">
+    <div class="grow z-20 bg-zinc-200 dark:bg-(#1D2132)">
       <div class="min-h-[calc(100vh-64px)] pb-5">
         <main class="py-[10px] px-[25px] w-full">
           <Toast />
