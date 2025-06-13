b42e64dc93ca49589dd05066756a03982e0458db
Alejandro Méndez <alejmendez.87@gmail.com>
2025-06-12 22:58:46 -0400

    Reorganizar las rutas de autenticación en web.php, eliminando el middleware 'guest' para las rutas de inicio de sesión y registro, y manteniendo la estructura de las rutas de recuperación de contraseña. Esto mejora la claridad y accesibilidad de las rutas en el módulo de autenticación.

--- Modules/Auth/Routes/web.php
+++ Modules/Auth/Routes/web.php
@@ -12,18 +12,15 @@ use Modules\Auth\Http\Controllers\RegisteredUserController;
 use Modules\Auth\Http\Controllers\VerifyEmailController;
 use Modules\Auth\Http\Controllers\Api\AuthenticatedApiController;
 
-Route::middleware('guest')->group(function () {
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
+Route::post('login', [AuthenticatedSessionController::class, 'store']);
+Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
+Route::post('register', [RegisteredUserController::class, 'store']);
 
-    Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
-    Route::post('register', [RegisteredUserController::class, 'store']);
-
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
-    Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.store');
-});
+Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
+Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
+Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
+Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.store');
 
 Route::middleware('auth')->group(function () {
     Route::get('verify-email', EmailVerificationPromptController::class)->name('verification.notice');
@@ -43,11 +40,8 @@ Route::middleware('auth')->group(function () {
 });
 
 
-Route::prefix('api')
-    ->group(function () {
-    Route::middleware('guest')->group(function () {
-        Route::post('auth/sign_in', [AuthenticatedApiController::class, 'store']);
-    });
+Route::prefix('api')->group(function () {
+    Route::post('auth/sign_in', [AuthenticatedApiController::class, 'store']);
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::get('auth/validate_token', [AuthenticatedApiController::class, 'validate_token']);
